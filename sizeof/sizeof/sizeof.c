7072f2628d38eb7200e9645712bab44687e5c197
ercankacmaz <ercankacmaz870870@gmail.com>
2023-02-22 11:06:18 +0300

    sizeof operator example

---
+++ sizeof/sizeof/sizeof.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+int main()
+{
+	printf("Size of Char: %zu\n", sizeof(char));
+	printf("Size of Int: %zu\n", sizeof(int));
+	printf("Size of Long: %zu\n", sizeof(long));
+	printf("Size of Long Long: %zu\n", sizeof(long long));
+	printf("Size of Float: %zu\n", sizeof(float));
+	printf("Size of Double: %zu\n", sizeof(double));
+	printf("Size of Long Double: %zu\n", sizeof(long double));
+
+
+
+
+
+}
